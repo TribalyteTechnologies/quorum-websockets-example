16a86ce4be770f8cc43cdf774a04b3e9c682deb0
Ronny <rdemera@tribalyte.com>
2019-07-05 13:59:00 +0200

    Unlock account from migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 const Migrations = artifacts.require("Migrations");
+const Web3 = require('web3');
+const enviroment = require('../environment')
 
 module.exports = function(deployer) {
+  let web3 = new Web3(enviroment.web3Provider_http);
+  web3.eth.personal.unlockAccount(enviroment.account, enviroment.accountPass, 600);
   deployer.deploy(Migrations);
 };
