f337fc6fee983b4aff4fe948f593ac7d573681a2
Ronny <rdemera@tribalyte.com>
2019-07-05 14:01:15 +0200

    Handle ws disconnects

--- eventListener.js
+++ eventListener.js
@@ -1,27 +1,43 @@
-var Web3 = require('web3');
-var contractAbi = require('./build/contracts/Profile.json');
+const Web3 = require('web3');
+const contractAbi = require('./build/contracts/Profile.json');
+const enviroment = require('./environment');
 
-var web3 = new Web3(new Web3.providers.WebsocketProvider("ws://0.0.0.0/ws", {
-    headers: {
-    Origin: "test.com"
-    }
-    }));
+var web3 = openConnection();
 
-
-web3.eth.net.isListening
-().then((res) => {
-    console.log('Is connected');
-}).catch(e => {
-    console.log('lost connection' + e);
-});
-
-
-const contractAddress = contractAbi.networks[83584648538].address;
-
+const contractAddress = contractAbi.networks[enviroment.netId].address;
 var contract = new web3.eth.Contract(contractAbi.abi, contractAddress);
 
-contract.events.changeProfile({
-    fromBlock: 0
-}, function(error, event) {
-    console.log("Change Profile Event: " + JSON.stringify(event.returnValues));
-})
+function newListener() {
+    console.log("New Subscription");
+    contract.events.changeProfile({
+        fromBlock: 0
+    }, function (error, event) {
+        if(event) {
+            console.log("Change Profile Event: " + JSON.stringify(event.returnValues));
+        } 
+        else if(error) {
+            console.log("Connection error: " + JSON.stringify(error));
+            web3 = openConnection();
+            contract = new web3.eth.Contract(contractAbi.abi, contractAddress);
+            newListener();
+        }
+    });
+}
+
+
+function openConnection() {
+    let auxWeb3 = new Web3(new Web3.providers.WebsocketProvider(enviroment.web3Provider_ws, {
+        headers: {
+            Origin: enviroment.originHeader
+        }
+    }));
+    auxWeb3.eth.net.isListening
+    ().then((res) => {
+        console.log('Is connected');
+    }).catch(e => {
+        console.log('Lost connection' + e);
+    });
+    return auxWeb3;
+}
+
+newListener();
